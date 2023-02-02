f7030e8d61afb756f4787ccd896a5b1fe6efc2ad
isaacomo <aas.isaac26@gmail.com>
2023-02-02 02:14:52 -0800

    fix

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,13 +1,2 @@
 #!/usr/bin/env ruby
-
-require 'oniguruma'
-
-regex = Oniguruma::ORegexp.new("School")
-
-input_string = ARGV[0]
-
-if regex.match(input_string)
-  puts "The input string matches the regular expression: School"
-else
-  puts "The input string does not match the regular expression: School"
-end
+puts ARGV[0].scan(/School/).join
