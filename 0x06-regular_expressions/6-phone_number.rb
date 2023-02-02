9a197b7743e65ab48861a39c9c0585b3c3bb063d
isaacomo <aas.isaac26@gmail.com>
2023-02-02 01:57:48 -0800

    fix 6

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -1,17 +1,2 @@
 #!/usr/bin/env ruby
-
-# Accept an argument from the command line
-argument = ARGV[0]
-
-# Define the regular expression pattern for a 10-digit phone number
-pattern = /^\d{10}$/
-
-# Pass the argument to the `match` method of the `Regexp` class
-result = pattern.match(argument)
-
-# Print the result of the match
-if result
-  puts "The argument is a 10-digit phone number."
-else
-  puts "The argument is not a 10-digit phone number."
-end
+puts ARGV[0].scan(/^\d{1,10}$/).join
